a4dcdcaaae08e2b33e436e6a2b3b1d378f8209ec
Md Arfan Khan <khan188993@gmail.com>
2022-01-11 07:26:00 +0600

    fresh-start-everythings

--- src/App.js
+++ src/App.js
@@ -1,55 +1,12 @@
-import logo from './logo.svg';
-import { useState } from 'react';
-import './App.css';
+import React, { useReducer } from 'react'
+import UseReducer from './react-crash/useReducer'
 
-function App(){
-
-  const [email, setEmail] = useState("")
-  const [password, setPassword] = useState("")
-  const [allData, setAllData] = useState([])
-
-  const submitForm = (e)=>{
-    e.preventDefault();
-    let newData = {email,password};
-    setEmail("");
-    setPassword("");
-    setAllData([...allData,newData]);
-    
-
-    
-  }
-  
-
-
-
-
-
-  return(
+const App = () => {
+  return (
     <>
-      <h1>Log IN form</h1>
-      <form action="" onSubmit={submitForm}>
-        email:<input value={email} type="email" onChange={(e)=>setEmail(e.target.value)}/><br></br>
-        password:<input value={password} type="password" onChange={(e)=>setPassword(e.target.value)}/><br></br>
-        <input type="submit" value="Login" /><br></br>
-      </form>
-
-      {
-        allData.map((current)=>{
-
-          return(
-            <div class="showData">
-             <h3>Email:{current.email}<br></br> Pass:{current.password}</h3>
-          </div>
-          )
-          
-        })
-      }
-
-     
-     
-      
+      <UseReducer/>
     </>
   )
 }
 
-export default App;
+export default App
---
+++ src/react-crash/useReducer.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const UseReducer = () => {
+    return (
+        <div>
+            <h1>UseReducer</h1>
+        </div>
+    )
+}
+
+export default UseReducer
