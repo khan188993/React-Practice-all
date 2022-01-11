2cb8d8cb9eb91d08f922fe9bfccc36fb8d708b56
Md Arfan Khan <khan188993@gmail.com>
2022-01-11 09:27:48 +0600

    8-useEffect axios add

--- src/App.js
+++ src/App.js
@@ -2,11 +2,12 @@ import React from 'react'
 import UseState from './react-crash/useState'
 import UseReducer from './react-crash/useReducer'
 import UseEffect from './react-crash/useEffect'
+import UseEffectApi from './react-crash/useEffectApi'
 
 const App = () => {
   return (
     <>
-      <UseEffect/>
+      <UseEffectApi/>
     </>
   )
 }
