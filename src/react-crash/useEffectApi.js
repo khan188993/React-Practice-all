641b23ae944ed8c5757393d65183de727cf66207
Md Arfan Khan <khan188993@gmail.com>
2022-01-11 09:43:21 +0600

    9-useEffect axios again

--- src/react-crash/useEffectApi.js
+++ src/react-crash/useEffectApi.js
@@ -2,30 +2,27 @@ import React,{useState,useEffect} from 'react'
 import axios from 'axios';
 
 const UseEffectApi = () => {
-    const [data, setData] = useState("")
+    const [data, setData] = useState([])
     useEffect(() => {
-        axios
-        .get('https://jsonplaceholder.typicode.com/users')
-        .then((response)=>{ 
-            setData(response.data)
+        axios.get('https://jsonplaceholder.typicode.com/users')
+        .then((response)=>{
+            setData(()=>response.data)
         })
-        
-    },[data])
+    }, [])
+
+
+
+
+
+
+    
     return (
         <>
-            {
-                data.length > 0 && 
-                data.map((value, index, array) => {
-                    if(array.length>0){
-                        return <>
-                        <h1>{value.name}</h1>
-                    <p>{value.id}</p>
-                    </>
-                    }
-                })
-                
-            }
-            <h1>UseEffectApi</h1>
+          {
+              data.map((value, index, array) => {
+                  return <h1 key={value.id} >Name : {value.name}</h1>
+              })
+          }
         </>
     )
 }
