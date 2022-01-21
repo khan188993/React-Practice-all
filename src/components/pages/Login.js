b7e147591a8096426c54c2e173dd6ea35be1acd5
Md Arfan Khan <khan188993@gmail.com>
2022-01-22 01:14:02 +0600

    5-log out with message,useLocation,useNavigator,

--- src/components/pages/Login.js
+++ src/components/pages/Login.js
@@ -1,16 +1,23 @@
-import React from 'react'
-import { useLocation } from 'react-router-dom'
+import React from "react";
+import { useLocation, useNavigate } from "react-router-dom";
 
 const Login = () => {
-    // console.log(useLocation());
-    const Location = useLocation();
-    return (
-        <>
-         <h1>Login Page</h1>   
-         {/* <h1>{Location.state && Location.state.st}</h1> */}
-         {Location.state && <h2>{Location.state.st}</h2>}
-        </>
-    )
-}
+  // console.log(useLocation());
+  const Location = useLocation();
+  const Navigate = useNavigate();
+  const data = {
+      name:"arfan khan"
+  }
+  return (
+    <>
+      <h1>Login Page</h1>
+      <button onClick={() => {
+          Navigate('/logout',{state:data}) //button click e navitor 
+      }}>Log Out</button>
+      {/* <h1>{Location.state && Location.state.st}</h1> */}
+      {Location.state && <h2>{Location.state.st}</h2>}
+    </>
+  );
+};
 
-export default Login
+export default Login;
--- src/components/pages/Logout.js
+++ src/components/pages/Logout.js
@@ -1,9 +1,13 @@
 import React from 'react'
+import { useLocation } from 'react-router-dom'
 
 const Logout = () => {
+    // console.log(useLocation());
+    const Location = useLocation();
     return (
         <>
          <h1>Logout Page</h1>   
+         {Location.state && <h1>{Location.state.name}</h1>}
         </>
     )
 }
